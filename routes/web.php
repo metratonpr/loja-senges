9ad1ce053935aa2ad98c139c01e3d388f0444694
metratonpr <julianovieira@yahoo.com.br>
2024-08-18 15:26:26 -0300

    Controllers

--- routes/web.php
+++ routes/web.php
@@ -19,40 +19,94 @@ Route::get('/', function () {
 
 
 // Pegar uma pagina e passar atributos via url
-Route::get('/empresa', function() {
+Route::get('/empresa', function () {
     return view('site/empresa');
 });
 
 //Permitir todos os tipos de acesso, acesso inseguro
-Route::any('/any', function(){
+Route::any('/any', function () {
     return "Permite todo tipo de acesso http (put, delete, get, post)";
 });
 
 //Inverso da any
-Route::match(['get','post'],'/match', function(){
+Route::match(['get', 'post'], '/match', function () {
     return "Permite apenas acessos definidos";
 });
 
 // Passagem de parametro, use ? e coloque um valor default para ser opcional
-Route::get("/parametro/{id}/{cat?}", function($id,$cat = ""){
-    return "o id do produto é: ".$id."<br>"."Categoria: ".$cat;
+Route::get("/parametro/{id}/{cat?}", function ($id, $cat = "") {
+    return "o id do produto é: " . $id . "<br>" . "Categoria: " . $cat;
 });
 
 
 //Redirect
-Route::get('/sobre', function(){
- return   redirect('/empresa');
+Route::get('/sobre', function () {
+    return   redirect('/empresa');
 });
 
-Route::redirect('/redirect','/empresa');
+Route::redirect('/redirect', '/empresa');
 
-Route::view('/view','site/empresa');
+Route::view('/view', 'site/empresa');
 
 //Rotas nomeadas
-Route::get('/news',function(){
+Route::get('/news', function () {
     return view('news');
 })->name('noticias');
 
-Route::get('/novidades',function(){
-return redirect()->route('noticias');
+Route::get('/novidades', function () {
+    return redirect()->route('noticias');
 });
+
+
+//Grupo de Rotas
+
+// Route::prefix('admin')->group(function(){
+
+//     Route::get('/dashboard', function(){
+//         return "dashboard";
+//     })->name('admin.dashboard');
+
+//     Route::get('/users', function(){
+//         return "users";
+//     })->name('admin.users');
+
+//     Route::get('/groups', function(){
+//         return "grupos";
+//     })->name('admin.groups');
+
+
+// });
+
+// Route::name('admin.')->group(function () {
+
+//     Route::get('/dashboard', function () {
+//         return "dashboard";
+//     })->name('dashboard');
+
+//     Route::get('/users', function () {
+//         return "users";
+//     })->name('users');
+
+//     Route::get('/groups', function () {
+//         return "grupos";
+//     })->name('groups');
+// });
+
+// as = nome das rotas do grupo
+Route::group(['prefix' => 'admin', 'as' => 'admin.'],function () {
+
+    Route::get('/dashboard', function () {
+        return "dashboard";
+    })->name('dashboard');
+
+    Route::get('/users', function () {
+        return "users";
+    })->name('users');
+
+    Route::get('/groups', function () {
+        return "grupos";
+    })->name('groups');
+});
+
+//Rota com controller
+//Route::get('/', [ProdutoController::class, 'index']);
